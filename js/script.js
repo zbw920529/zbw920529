53712986545cc2d8bfddd2cf8fe1c4cc81ab7849
zbw920529 <97155429@qq.com>
2018-09-29 17:05:34 +0800

    ce

--- js/script.js
+++ js/script.js
@@ -1,54 +1,18 @@
-//菜单栏的显示  children()返回返回被选元素的所有直接子元素
-$("#banner_menu_wrap").children().hover(function(){
-    $(this).css("background","#ff6700");
-    $(this).children(".banner_menu_content").css("border","1px solid #F0F0F0").show();
-},function(){
-    $(this).css("background","none");
-    $(this).children(".banner_menu_content").css("border","0px solid #F0F0F0").hide();
-});
-//轮播
 $(function(){
-    var i=0;
-    var big_banner_pic = $("#big_banner_pic");
-    var allimg=$("#big_banner_pic li").length;
-    function img_change(){
-        var img_i=i*-document.body.clientWidth;
-        big_banner_pic.animate({opacity:".2"},100,function(){
-            big_banner_pic.css("left",img_i );
-            big_banner_pic.animate({
-                opacity: "1"
-            }, 1000);
+            //锚点跳转滑动效果
+            $('a[href*=#],area[href*=#]').click(function() {
+                console.log(this.pathname)
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top;
+                        $('html,body').animate({
+                                    scrollTop: targetOffset
+                                },
+                                500);
+                        return false;
+                    }
+                }
+            });
         })
-    }
-    //自动的
-    function automatic(){
-        i+=1;
-        if(i>=allimg){
-            i=0;
-        }
-        img_change();
-    }
-    change_self_time = setInterval(automatic, 3000);
-    //轮播上一张下一张图标控制
-    $("#big_banner_change_wrap").hover(function(){
-        clearInterval(change_self_time);
-        $("#big_banner_change_wrap").children().show();
-    },function(){
-        change_self_time = setInterval(automatic, 3000);
-        $("#big_banner_change_wrap").children().hide();
-    })
-    $("#big_banner_change_prev").click(function(){
-        i += 1;
-        if (i >= allimg) {
-            i = 0;
-        }
-        img_change();
-    })
-    $("#big_banner_change_next").click(function(){
-        i -= 1;
-        if (i <= 0) {
-            i = allimg - 1;
-        }
-        img_change();
-    })
-})
